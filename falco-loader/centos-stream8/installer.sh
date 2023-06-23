27fdb0e9d5aed4f825605d4ddc08164d528bed39
Nithish <nithishkarthik01@gmail.com>
2023-06-23 16:00:45 +0530

    fix: Make sure the directory is available on the node
    Signed-off-by: Nithish <nithishkarthik01@gmail.com>

--- falco-loader/centos-stream8/installer.sh
+++ falco-loader/centos-stream8/installer.sh
@@ -1,3 +1,4 @@
 dnf install -y kernel-devel-$(uname -r)
 dnf install -y make clang llvm
+mkdir /usr/_falco/ && mv falco-0.35.0-x86_64 /usr/_falco/falco
 /usr/_falco/falco/usr/bin/falco-driver-loader bpf
