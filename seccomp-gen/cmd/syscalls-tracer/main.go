cfb3a707ff2ef6cc8253e072d8f53c271eeaef7a
Nithish <nithishkarthik01@gmail.com>
2023-06-26 15:25:20 +0530

    feat: Add placeholders
    Signed-off-by: Nithish <nithishkarthik01@gmail.com>

--- seccomp-gen/cmd/syscalls-tracer/main.go
+++ seccomp-gen/cmd/syscalls-tracer/main.go
@@ -8,12 +8,22 @@ import (
 
 func startTraceHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Hello")
-	// Run falco with appropriate configuration
+	// Run falco in such a way that it will start tracing with:
+	// 1. The given set of rules
+	// 2. Using program_output to stream the output to the falco-syscalls-formatter binary with appropriate arguments
+	// 3. Kubernetes option enabled
+	// 4. Set to json_output
+	// With the following flags:
+	// 1. -A for monitoring all the syscall events
+	// 2. --k8s-api
+	// 3. --k8s-api-cert
+	// 4. --unbuffered
+
 }
 
 func stopTracingHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Hello")
-	// Stop falco and send the resultant syscalls
+	// Stop falco and send the resultant syscalls data.json file
 }
 
 func main() {
