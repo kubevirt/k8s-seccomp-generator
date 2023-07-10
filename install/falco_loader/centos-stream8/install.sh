392f39f10f80af8f7a0aa778e23721330198b91b
Nithish Karthik <nithishkarthik01@gmail.com>
2023-07-10 16:07:46 +0530

    feat: automate the installation process

--- install/falco_loader/centos-stream8/install.sh
+++ install/falco_loader/centos-stream8/install.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 dnf install -y kernel-devel-$(uname -r)
 dnf install -y make clang llvm
 mkdir -p /usr/_falco/falco && cp -R falco-0.35.0-x86_64/* /usr/_falco/falco/ && /usr/_falco/falco/usr/bin/falco-driver-loader bpf
--- install/install.sh
+++ install/install.sh
@@ -1,3 +1,20 @@
+#!/bin/bash
+if [ -z "$1" ]
+  then
+    echo "Please specify the OS distribution of the node"
+fi
+nodeos="$1"
+kubectl apply -f falco_loader/$nodeos/falco_loader_pod.yaml
+if [ $? -ne 0 ]; then
+  exit 1
+fi
+echo "Waiting for falco loader pod to complete. This might take 5 mins..."
+kubectl wait --for=condition=complete pod/falco-loader --timeout=300s
+if [ $? -ne 0 ]; then
+  exit 1
+fi
+
+echo "Starting syscalls tracer..."
 kubectl create ns kubesecgen
 kubectl apply -f clusterrole.yaml
 kubectl apply -f serviceaccount.yaml
