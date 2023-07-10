9b29c4cc5bd092d9c4782457a47819a47d5ffa6d
Nithish Karthik <nithishkarthik01@gmail.com>
2023-07-10 15:28:09 +0530

    refactor: tidy up the formatter

--- seccomp_gen/cmd/falco_syscalls_formatter/main.go
+++ seccomp_gen/cmd/falco_syscalls_formatter/main.go
@@ -10,7 +10,10 @@ import (
 )
 
 /*
-  This binary takes the falco syscalls stream as the input and formats them and writes them to a file
+ * This binary takes the falco syscalls stream as the input (from stdio) and formats them and writes them to a file
+ * Requirements: 
+ * 1. Falco `json_output` has to be enabled
+ * 2. Falco rule should have `syscall.type` in the output
 */
 
 type falcoOutput struct {
@@ -23,24 +26,18 @@ type falcoOutput struct {
   OutputFields map[string]interface{} `json:"output_fields"`
 }
 
-
 func main() {
   fmt.Println("Starting formatter...")
   scanner := bufio.NewScanner(os.Stdin)
   syscallsMap := make(map[string]struct{})
   c := make(chan os.Signal, 1)
+  // we use SIGURG since Falco will be spawning this binary and it sends SIGURG when killed 
+  // https://falco.org/docs/alerts/channels/#program-output - look at `Controlling the program output` section for more info
   signal.Notify(c, syscall.SIGURG)
   go func(c chan os.Signal){
-    fmt.Println("waiting for signal...")
-    sig := <-c
-    fmt.Printf("Caught %s, exiting... ", sig.String())
-    // write the syscalls to a data.json file
-    syscallData := make([]string, 0)
-    for syscall := range syscallsMap {
-      syscallData = append(syscallData, syscall)
-    }
-    file, _ := json.Marshal(syscallData)
-    _ = os.WriteFile("/falco/data.json", file, 0644)
+    <-c
+    fmt.Printf("Received SIGURG, exiting... ")
+    writeSyscallsData(syscallsMap) 
     os.Exit(0)
   }(c)
   for scanner.Scan() {
@@ -48,7 +45,7 @@ func main() {
         text := scanner.Text()
         err := json.Unmarshal([]byte(text), &val)
         if err != nil {
-          fmt.Println("Error : ", err)
+          fmt.Println("Cannot unmarshal input: ", err)
         }
         syscall, ok :=  val.OutputFields["syscall.type"].(string)
         if ok && syscall != "" {
@@ -61,4 +58,12 @@ func main() {
 	}
 }
 
-
+// write the syscalls to a data.json file
+func writeSyscallsData(syscallsMap map[string]struct{}) {
+    syscallData := make([]string, 0)
+    for syscall := range syscallsMap {
+      syscallData = append(syscallData, syscall)
+    }
+    file, _ := json.Marshal(syscallData)
+    _ = os.WriteFile("/falco/data.json", file, 0644)
+}
