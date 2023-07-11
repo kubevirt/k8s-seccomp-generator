d1502d03c2d3b2d05f449da0f221cbe54007e066
Nithish Karthik <nithishkarthik01@gmail.com>
2023-07-11 11:54:28 +0530

    fix: Change waiting condition to Completed

--- scripts/install.sh
+++ scripts/install.sh
@@ -8,18 +8,18 @@ kubectl apply -f ../install/falco_loader/$nodeos/falco_loader_pod.yaml
 if [ $? -ne 0 ]; then
   exit 1
 fi
-echo "Waiting for falco loader pod to complete. This might take 5 mins..."
-kubectl wait --for=condition=complete pod/falco-loader --timeout=300s
+echo "Waiting for falco loader pod to complete. This might take 6 mins..."
+kubectl wait --for=condition=Completed pod/falco-loader --timeout=360s
 if [ $? -ne 0 ]; then
   exit 1
 fi
 
 echo "Starting syscalls tracer..."
 kubectl create ns kubesecgen
-kubectl apply -f clusterrole.yaml
-kubectl apply -f serviceaccount.yaml
-kubectl apply -f clusterrolebinding.yaml
-kubectl apply -f syscalls-tracer-pod.yaml
-kubectl apply -f syscalls-tracer-service.yaml
+kubectl apply -f ../install/clusterrole.yaml
+kubectl apply -f ../install/serviceaccount.yaml
+kubectl apply -f ../install/clusterrolebinding.yaml
+kubectl apply -f ../install/syscalls-tracer-pod.yaml
+kubectl apply -f ../install/syscalls-tracer-service.yaml
 kubectl get service/syscalls-tracer -n kubesecgen
 
